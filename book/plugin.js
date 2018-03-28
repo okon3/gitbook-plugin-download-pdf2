f1a239db437c92052a28e86d3abb76e54e9d7884
Luca Regazzi <l.regazzi@iconsultig.biz>
2018-03-28 22:14:49 +0200

    Fixed a bug in reading the configurations

--- book/plugin.js
+++ book/plugin.js
@@ -1,6 +1,6 @@
 require(["gitbook", "jQuery"], function (gitbook, $) {
   gitbook.events.bind('start', function (e, config) {
-    var conf = config.editlink
+    var conf = config['get-pdf-link'];
     var label = conf.label
     var base = conf.base
 
