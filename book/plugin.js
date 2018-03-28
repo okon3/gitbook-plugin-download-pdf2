c0caaf75cdc434022616d66f84501bba6d8fb935
Luca Regazzi <l.regazzi@iconsultig.biz>
2018-03-28 21:56:40 +0200

    first commit

---
+++ book/plugin.js
@@ -0,0 +1,16 @@
+require(["gitbook", "jQuery"], function (gitbook, $) {
+  gitbook.events.bind('start', function (e, config) {
+    var conf = config.editlink
+    var label = conf.label
+    var base = conf.base
+
+    gitbook.toolbar.createButton({
+      icon: 'fa fa-pdf',
+      text: label,
+      onClick: function () {
+        var filepath = gitbook.state.filepath
+        window.open(base + filepath)
+      }
+    })
+  })
+})
